9d2508838fe0572b8c7dcfbabd25fd4fc5ebc27e
tomashege <41945595+tomashege@users.noreply.github.com>
2019-01-16 12:15:35 +0200

    fixed Tribonacci

--- Tribonacci.java
+++ Tribonacci.java
@@ -2,24 +2,23 @@ public class Tribonacci {
     public static void main(String[] args) {
       String r = "T(n-1) + T(n-2) + T(n-3)";
 
-      for (int n= 0;n<14;n++) {
-        System.out.println(method(n));
+      for (int n= 0;n<15;n++) {
+        System.out.println("T("+n+")=="+t(n));
       }
     }
-      public static String method(int n){
-        String temp = "";
+      public static int t(int n){
+        int temp = 0;
         if (n== 0){
-          temp = "T(0)==0";
+          temp = 0;
         }
         else if (n == 1){
-          temp = "T(1)==1";
+          temp = 1;
         }
         else if (n == 2){
-          temp = "T(2)==1";
+          temp =1;
         }
         else{
-          int num = (n-1) + (n-2) + (n-3);
-          temp= "T("+n+")=="+num;
+          temp = t(n-1) + t(n-2) + t(n-3);
 
         }
         return temp;
